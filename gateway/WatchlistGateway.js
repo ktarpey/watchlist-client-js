dc575449e4f4966d99fc80fafc0b1c8da869a636
Bryan Ingle <bryan.ingle@barchart.com>
2018-01-24 16:02:26 -0600

    WIP

--- gateway/WatchlistGateway.js
+++ gateway/WatchlistGateway.js
@@ -3,7 +3,12 @@ const assert = require('@barchart/common-js/lang/assert'),
 	Serializer = require('@barchart/common-js/timing/Serializer');
 
 const EndpointBuilder = require('@barchart/common-client-js/http/builders/EndpointBuilder'),
-	WatchlistUser = require('@barchart/watchlist-api-common/WatchlistUser');
+	Gateway = require('@barchart/common-client-js/http/Gateway'),
+	PathParameterType = require('@barchart/common-client-js/http/definitions/PathParameterType'),
+	RequestInterceptor = require('@barchart/common-client-js/http/interceptors/RequestInterceptor'),
+	ResponseInterceptor = require('@barchart/common-client-js/http/interceptors/ResponseInterceptor');
+
+const WatchlistUser = require('@barchart/watchlist-api-common/WatchlistUser');
 
 const WatchlistGatewayConfiguration = require('./WatchlistGatewayConfiguration');
 
@@ -26,19 +31,40 @@ module.exports = (() => {
 			this._serializer = new Serializer();
 		}
 
-		start(configuration, requestInterceptor, responseInterceptor) {
+		start(configuration) {
 			return Promise.resolve()
 				.then(() => {
 					if (this._startPromise !== null) {
 						return this._startPromise;
 					}
 
-					assert.argumentIsRequired(configuration, 'configuration', WatchlistGatewayConfiguration, 'WatchlistGatewayConfiguration');
+					assert.argumentIsOptional(configuration, 'configuration', WatchlistGatewayConfiguration, 'WatchlistGatewayConfiguration');
+
+					const configurationToUse = configuration || WatchlistGatewayConfiguration.DEFAULT;
 
 					this._startPromise = Promise.resolve()
 						.then(() => {
-							this._readEndpoint = null;
-							this._writeEndpoint = null;
+							this._readEndpoint = EndpointBuilder.for('read-user')
+								.withVerb(VerbType.GET)
+								.withProtocol(configurationToUse.protocol)
+								.withHost(configurationToUse.host)
+								.withPort(configurationToUse.port)
+								.withPathParameter('v1', PathParameterType.STATIC)
+								.withPathParameter('user', PathParameterType.STATIC)
+								.withRequestInterceptor(configuration.requestInterceptorForJwt)
+								.withResponseInterceptor(responseInterceptorForDeserialization)
+								.endpoint;
+
+							this._writeEndpoint = EndpointBuilder.for('write-user')
+								.withVerb(VerbType.PUT)
+								.withProtocol(configurationToUse.protocol)
+								.withHost(configurationToUse.host)
+								.withPort(configurationToUse.port)
+								.withPathParameter('v1', PathParameterType.STATIC)
+								.withPathParameter('user', PathParameterType.STATIC)
+								.withRequestInterceptor(configuration.requestInterceptorForJwt)
+								.withRequestInterceptor(requestInterceptorForSerialization)
+								.endpoint;
 
 							return this._started = true;
 						});
@@ -51,6 +77,11 @@ module.exports = (() => {
 					if (!this._started) {
 						throw new Error('Unable to write watchlist user, the gateway has not started.');
 					}
+
+					return Gateway.invoke(this._readEndpoint)
+						.then((response) => {
+
+						});
 				});
 		}
 
@@ -62,6 +93,11 @@ module.exports = (() => {
 					if (!this._started) {
 						throw new Error('Unable to write watchlist user, the gateway has not started.');
 					}
+
+					return Gateway.invoke(this._writeEndpoint, watchlistUser)
+						.then((response) => {
+
+						});
 				});
 		}
 
@@ -69,6 +105,18 @@ module.exports = (() => {
 			return `[WatchlistGateway]`;
 		}
 	}
+
+	const requestInterceptorForSerialization = RequestInterceptor.fromDelegate((request) => {
+		assert.argumentIsRequired(request.data, 'request.data', WatchlistUser, 'WatchlistUser');
+
+		request.data = request.data.toJSObj();
+
+		return request;
+	});
+
+	const responseInterceptorForDeserialization = ResponseInterceptor.fromDelegate((request) => {
+		return WatchlistUser.fromJSObj(request.data);
+	});
 
 	return WatchlistGateway;
 })();
--- gateway/WatchlistGatewayConfiguration.js
+++ gateway/WatchlistGatewayConfiguration.js
@@ -1,5 +1,11 @@
 const assert = require('@barchart/common-js/lang/assert'),
+	Enum = require('@barchart/common-js/lang/Enum'),
 	is = require('@barchart/common-js/lang/is');
+
+const ProtocolType = require('@barchart/common-client-js/http/definitions/ProtocolType'),
+	VerbType = require('@barchart/common-client-js/http/definitions/VerbType');
+
+const RequestInterceptor = require('@barchart/common-client-js/http/interceptors/RequestInterceptor');
 
 module.exports = (() => {
 	'use strict';
@@ -8,36 +14,80 @@ module.exports = (() => {
 	 * Configuration data for a {@link WatchlistGateway}.
 	 *
 	 * @public
+	 * @param {String} host
+	 * @param {String} protocol
+	 * @param {Number} port
+	 * @param {Function} jwtProvider
 	 */
 	class WatchlistGatewayConfiguration {
-		constructor(protocol, host, port, requestInterceptorForRead) {
-			this._protocol = protocol;
+		constructor(host, protocol, port, jwtProvider) {
 			this._host = host;
-			this._port = port;
 
-			this._requestInterceptorForRead = requestInterceptorForRead || null;
+			this._protocol = Enum.fromCode(ProtocolType, protocol) || ProtocolType.HTTPS;
+			this._port = port || this._protocol.defaultPort;
+
+			this._requestInterceptorForJwt = RequestInterceptor.fromDelegate((request) => {
+				return Promise.resolve()
+					.then(() => {
+						return jwtProvider();
+					}).then((token) => {
+						request.headers.Authorization = `Bearer ${token}`;
+
+						return request;
+					});
+			});
 		}
 
+		/**
+		 * The {@link ProtocolType} to use for outgoing requests.
+		 *
+		 * @public
+		 * @returns {ProtocolType}
+		 */
 		get protocol() {
 			return this._protocol;
 		}
 
+		/**
+		 * The remote host to use for outgoing requests.
+		 *
+		 * @public
+		 * @returns {String}
+		 */
 		get host() {
 			return this._host;
 		}
 
+		/**
+		 * The TCP port to use for outgoing requests.
+		 *
+		 * @public
+		 * @returns {String}
+		 */
 		get port() {
 			return this._port;
 		}
 
-		get requestInterceptorForRead() {
-			return this._requestInterceptorForRead;
+		/**
+		 * A {@link RequestInterceptor} that adds a JWT token to outgoing requests.
+		 *
+		 * @public
+		 * @returns {RequestInterceptor}
+		 */
+		get requestInterceptorForJwt() {
+			return this._requestInterceptorForJwt;
+		}
+
+		static get DEFAULT() {
+			return defaultConfiguration;
 		}
 
 		toString() {
 			return `[WatchlistGatewayConfiguration]`;
 		}
 	}
+
+	const defaultConfiguration = new WatchlistGatewayConfiguration('tun13wxuxf.execute-api.us-east-1.amazonaws.com/prod', 'https');
 
 	return WatchlistGatewayConfiguration;
 })();
