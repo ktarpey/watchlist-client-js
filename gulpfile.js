415a56ac90ce4e00a7289a00153e64cd5cbfaf37
Bryan Ingle <bryan.ingle@barchart.com>
2018-02-08 14:58:15 -0600

    Exclude example bundle from linting

--- gulpfile.js
+++ gulpfile.js
@@ -138,7 +138,7 @@ gulp.task('release', (cb) => {
 });
 
 gulp.task('lint', () => {
-	return gulp.src([ './**/*.js', './test/specs/**/*.js', '!./node_modules/**', '!./docs/**', '!./test/SpecRunner.js' ])
+	return gulp.src([ './**/*.js', './test/specs/**/*.js', '!./node_modules/**', '!./docs/**', '!./test/SpecRunner.js', '!./example/example.js' ])
 		.pipe(jshint({'esversion': 6}))
 		.pipe(jshint.reporter('default'));
 });
