c772fcf18ef90298472221e0cd059c9f40deab2f
Bryan <bryan@Bryans-MacBook-Pro-2.local>
2020-07-25 19:18:54 -0500

    Change "embed-version" gulp script to update doc cover page

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ const AWS = require('aws-sdk'),
 	glob = require('glob'),
 	jasmine = require('gulp-jasmine'),
 	jshint = require('gulp-jshint'),
+	merge = require('merge-stream'),
 	prompt = require('gulp-prompt'),
 	rename = require('gulp-rename'),
 	replace = require('gulp-replace'),
@@ -60,11 +61,17 @@ gulp.task('bump-version', (cb) => {
 });
 
 gulp.task('embed-version', () => {
-	let version = getVersionFromPackage();
+	const version = getVersionFromPackage();
 
-	return gulp.src(['./lib/index.js'])
+	const meta = gulp.src(['./lib/index.js'])
 		.pipe(replace(/(version:\s*')([0-9]+\.[0-9]+\.[0-9]+)(')/g, '$1' + version + '$3'))
 		.pipe(gulp.dest('./lib/'));
+
+	const coverpage = gulp.src(['./docs/_coverpage.md'])
+		.pipe(replace(/[0-9]+\.[0-9]+\.[0-9]+/g, version))
+		.pipe(gulp.dest('./docs/'));
+
+	return merge(meta, coverpage);
 });
 
 gulp.task('build-example-bundle', run('npm run build'));
